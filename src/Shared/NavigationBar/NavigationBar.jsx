f0e6e0675fcc14a00dd9a967418f592d0f076f5f
Mohammad Apon <umar426.net@gmail.com>
2023-05-02 14:44:02 +0600

    banner added

--- src/Shared/NavigationBar/NavigationBar.jsx
+++ src/Shared/NavigationBar/NavigationBar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
-        <div className='flex justify-between items-center'>
+        <div className='lg:flex bg-gray-100 p-4 rounded-md justify-between items-center'>
             <div>
-                <h2 className='text-3xl font-semibold'>Prawn Hub!</h2>
+                <h2 className='text-3xl font-semibold lg:text-start text-center'><span className='text-yellow-500'>Fat</span> Chef</h2>
             </div>
-            <div className='flex gap-8 font-semibold items-center'>
+            <div className='flex lg:mx-0 mx-7 gap-8 font-semibold items-center'>
                 <Link to='/'>Home</Link>
                 <Link to='/blog'>Blog</Link>
                 <Link>User</Link>

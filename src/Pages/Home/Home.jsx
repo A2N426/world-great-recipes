5805be2743195bf26ccf6583c2ab979cb5fbd1e9
Mohammad Apon <umar426.net@gmail.com>
2023-05-04 18:35:44 +0600

    lazy loader added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import Banner from '../../components/Banner/Banner';
-import Chef from '../../components/Chef/Chef'
+// import Chef from '../../components/Chef/Chef'
 import Food from '../../components/Food/Food';
 import Latest from '../../components/Latest/Latest';
+const Chef = lazy(() => import('../../components/Chef/Chef'))
 
 const Home = () => {
-    
+
     return (
         <>
             <Banner></Banner>
-            <Chef></Chef>
+            <Suspense fallback={<div className='text-2xl mt-10 font-semibold'>Please Wait...</div>}>
+                <Chef></Chef>
+            </Suspense>
             <Food></Food>
             <Latest></Latest>
         </>
--- src/layouts/LoginLayout.jsx/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx/LoginLayout.jsx
@@ -1,13 +1,11 @@
-import React,{Suspense,lazy} from 'react';
-// import Main from '../Main/Main';
-const Main = lazy(()=>import('../Main/Main'))
+import React, { Suspense, lazy } from 'react';
+import Main from '../Main/Main';
+// const Main = lazy(()=>import('../Main/Main'))
 
 const LoginLayout = () => {
     return (
         <>
-            <Suspense fallback={<div className='text-2xl mt-10 font-semibold'>Please Wait...</div>}>
-                <Main></Main>
-            </Suspense>
+            <Main></Main>
         </>
     );
 };
